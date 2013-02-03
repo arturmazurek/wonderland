44e18ce0fd08ecd8d43048b13eba3b09b70db63e
Artur Mazurek <artur.s.mazurek@gmail.com>
2013-02-03 12:39:06 +0000

    Initial Commit

---
+++ CodeaMac/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  CodeaMac
+//
+//  Created by Artur Mazurek on 03.02.2013.
+//  Copyright (c) 2013 Artur Mazurek. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ CodeaMac/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CodeaMac
+//
+//  Created by Artur Mazurek on 03.02.2013.
+//  Copyright (c) 2013 Artur Mazurek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
