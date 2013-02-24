ce53acdfdd0260e186f8573853ceecc3a6c70b8f
Artur Mazurek <artur.s.mazurek@gmail.com>
2013-02-24 15:47:07 +0000

    Added shared pointer empty file

---
+++ Util/SharedPtr.h
@@ -0,0 +1,14 @@
+//
+//  SharedPtr.h
+//  Wonderland
+//
+//  Created by Artur Mazurek on 24.02.2013.
+//  Copyright (c) 2013 Artur Mazurek. All rights reserved.
+//
+
+#ifndef Wonderland_SharedPtr_h
+#define Wonderland_SharedPtr_h
+
+
+
+#endif
