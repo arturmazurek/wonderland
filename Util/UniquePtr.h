9e4b5768744b0dfeb406c1dd2f24cd8f594aa48c
Artur Mazurek <artur.s.mazurek@gmail.com>
2013-02-24 12:59:40 +0000

    Added UniquePtr stub

---
+++ Util/UniquePtr.h
@@ -0,0 +1,20 @@
+//
+//  UniquePtr.h
+//  Wonderland
+//
+//  Created by Artur Mazurek on 24.02.2013.
+//  Copyright (c) 2013 Artur Mazurek. All rights reserved.
+//
+
+#ifndef Wonderland_UniquePtr_h
+#define Wonderland_UniquePtr_h
+
+template <typename T>
+class UniquePtr {
+public:
+    explicit UniquePtr(T* obj = NULL) {
+        
+    }
+};
+
+#endif
