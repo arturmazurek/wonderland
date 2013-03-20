79f88a88bf2e7bc9520744e1b112b76ef146ea44
Artur Mazurek <artur.mazurek@apptivation.co.uk>
2013-03-20 16:27:14 +0000

    Added rotator file

---
+++ Math/Rotator.h
@@ -0,0 +1,14 @@
+//
+//  Rotator.h
+//  Wonderland
+//
+//  Created by Artur Mazurek on 20/03/2013.
+//  Copyright (c) 2013 Artur Mazurek. All rights reserved.
+//
+
+#ifndef Wonderland_Rotator_h
+#define Wonderland_Rotator_h
+
+
+
+#endif
